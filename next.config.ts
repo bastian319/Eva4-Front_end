8bee81f464a761125ad040bdc75c684e39f0515d
bastian319 <bastian.vargas20@inacapmail.cl>
2026-07-17 11:17:55 -0400

    Cambios en el next.config.ts
    se cambio el inicial nex.config.ts y lo actualize

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
+const repo = "Laboratorio_Next_React";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath: `/${repo}`,
+  assetPrefix: `/${repo}`,
+  trailingSlash: true,
 };
 
 export default nextConfig;
